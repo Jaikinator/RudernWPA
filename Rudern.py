1d0f95b5683c8914d649f88be7c535211cc59c67
Jaikinator <schmieder.jacob@web.de>
2020-06-20 12:37:24 +0200

    Visualiesierung

--- Rudern.py
+++ Rudern.py
@@ -12,9 +12,19 @@ time_sim = np.arange(0, (len(Dataframe["aX"]))) # zeitsimulation
 print(Dataframe) # print der Daten
 print(Dataframe.columns.values) # Print der Spaltennamen
 
+
+# Grafische Darstellung der Daten
+
+plt.subplot(211)
 plt.plot(time_sim, Dataframe["gX"], label = "gyro X")
 plt.plot(time_sim, Dataframe["gY"], label = "gyro Y")
 plt.plot(time_sim, Dataframe["gZ"], label = "gyro Z")
+plt.legend()
+
+plt.subplot(212)
+plt.plot(time_sim, Dataframe["aX"], label = "acc X")
+plt.plot(time_sim, Dataframe["aY"], label = "acc Y")
+plt.plot(time_sim, Dataframe["aZ"], label = "acc Z")
 plt.legend()
 plt.show()
 
