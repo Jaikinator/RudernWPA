bc9e0db3b32dffd6dbe3ca00f87772a282d6f19f
Jaikinator <schmieder.jacob@web.de>
2020-09-17 12:40:25 +0200

    daten anpassen

--- Rudern_test.py
+++ Rudern_test.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 
 col_names = ["aX","aY","aZ","temp","gX", "gY", "gZ"]  # festlegen welche Daten importiert werden
 
-Dataframe = pd.read_excel("RUDERN_Messung.xlsx") #einlesen der Daten
+Dataframe = pd.read_table("Testmessung1.txt", delim_whitespace = True, header=None ,usecols= [2,6,10,14,18,22,26],names= col_names) #einlesen der Daten
 
-#time_sim = np.arange(0, (len(Dataframe["aX"]))) # zeitsimulation
+time_sim = np.arange(0, (len(Dataframe["aX"]))) # zeitsimulation
 
 
 print(Dataframe) # print der Daten
 print(Dataframe.columns.values) # Print der Spaltennamen
 
-#velocitiy_x = np.trapz(Dataframe["aX"].to_numpy())
+velocitiy_x = np.trapz(Dataframe["aX"].to_numpy())
 # Grafische Darstellung der Daten
-#print(velocitiy_x )
+print(velocitiy_x )
 #plt.plot(time_sim,velocitiy_x)
 #plt.show()
 '''
